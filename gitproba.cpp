b2efd82d0b68054c4208444a33e4a5389d4fb300
Ekaterina Gnitii <kat.gni2004@gmail.com>
2023-04-05 00:21:06 +0000

    second commit, fibonachhi

--- gitproba.cpp
+++ gitproba.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n){ 
+    int a, b, sum, count; 
+    a = count = sum = 0; 
+    b = 1; 
+    while (count < n) { 
+        sum = a + b; 
+        a = b; 
+        b = sum; 
+        count++; 
+    } 
+    return a; 
+}
+
 int main(){
-	cout << "Hello, World!";
+	cout << "Hello, World!" << endl;
+	int n;
+	cout << "n="; cin >> n;
+	cout << fib(n);
 }
